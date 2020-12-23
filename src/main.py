c497e82bca4048e3fe5fef96383913444ee21440
Steven Stalder <steven.stalder30@gmail.com>
2020-12-23 16:38:21 +0100

    Added callback for early stopping

--- src/main.py
+++ src/main.py
@@ -2,14 +2,26 @@ import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from __init__ import *
 
 """
 Pytorch Lightning training, validation and testing here. Maybe with visualizations.
 """
 
+### Define Early Stopping condition ###
+early_stop_callback = EarlyStopping(
+   monitor='val_acc',
+   min_delta=0.001,
+   patience=2,
+   verbose=False,
+   mode='max'
+)
+
+### Define Model ###
 model = Protein_GRU_Sequencer()
 
-trainer = pl.Trainer(logger=False)
+### Train Model ###
+trainer = pl.Trainer(logger=False, callbacks=[early_stop_callback])
 trainer.fit(model, train_loader, val_loader)
 
