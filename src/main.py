acdcaf78413a9d56b1f676eea107c51d5c23ae8c
Steven Stalder <steven.stalder30@gmail.com>
2020-12-23 21:36:02 +0100

    Small bugfix for testing + changed stopping criterium for training

--- src/main.py
+++ src/main.py
@@ -12,8 +12,8 @@ Pytorch Lightning training, validation and testing here. Maybe with visualizatio
 ### Define Early Stopping condition ###
 early_stop_callback = EarlyStopping(
    monitor='val_acc',
-   min_delta=0.001,
-   patience=2,
+   min_delta=0.0001,
+   patience=20,
    verbose=False,
    mode='max'
 )
@@ -25,4 +25,4 @@ model = Protein_GRU_Sequencer()
 trainer = pl.Trainer(logger=False, callbacks=[early_stop_callback], gpus=torch.cuda.device_count())
 trainer.fit(model, train_loader, val_loader)
 
-trainer.test(test_dataloader=test_loader)
+trainer.test(test_dataloaders=test_loader)
